dfb1d14fa8f635f68a9de24a577423453e96d2f1
CarlaOnate <carlaonategardella@gmail.com>
2022-09-12 14:16:59 -0500

    inicio registro, ejemplo de antdesign e instalacion

--- pagina-web/src/Routing.js
+++ pagina-web/src/Routing.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './pages/clasesReact/Home/App';
 import Pepo from './pages/clasesReact/Pepo'
 import { Ivan } from './pages/clasesReact/Ivan'
+import Registration from './pages/Registration'
 import RickAndMorty from './pages/clasesReact/RickAndMortyApi'
 import { Routes, Route } from "react-router-dom";
 
@@ -9,6 +10,7 @@ function Routing() {
   return (
     <Routes>
       <Route  path="/" element={<Home />}/>
+      <Route  path="/registro" element={<Registration />}/>
       <Route  path="/jiji" element={<p> JIJIJI </p>}/>
       <Route  path="/pepo" element={<Pepo />}/>
       <Route  path="/ivan" element={<Ivan />}/>
