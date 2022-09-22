1bf99ccfa4fccce3af1989f17374adf4d2066441
Ivan Alvarez <ivan15alvarez@outlook.com>
2022-09-21 20:29:36 -0500

    Se termina tentativamente la funcionalidad de editar usuarios

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
@@ -9,4 +10,9 @@ urlpatterns = [
   path('logged-user', views.getLoggedUser, name='getLoggedUser'),
 
   path('edit-user', views.editUserAdmin, name='editUserAdmin'),
+
+  path('delete-user', views.deleteUser, name='deleteUser'),
+
+  path('getuseritself-user', views.getuseritself, name='getuseritself'),
+
 ]
