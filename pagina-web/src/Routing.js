2072bd524793d1d3fd63f6b0885364307a53dbd4
CarlaOnate <carlaonategardella@gmail.com>
2022-10-01 14:00:11 -0500

    modified routing, added functional navbar with classnames

--- pagina-web/src/Routing.js
+++ pagina-web/src/Routing.js
@@ -2,71 +2,71 @@ import React, {useContext} from 'react';
 import Contra from './Componentes/Contra';
 import imagen1 from './Imagenes/Imagen1.png';
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
-import Navbar from './Componentes/Navbars/Navbar';
+import { Navbar2 } from './Componentes/Navbars/Navbar2';
 import Pantallaprincipal from './Componentes/pantallaPrincipal';
 import Reservas from './Componentes/Reservas';
 import IniciarSesion from './Componentes/IniciarSesion';
 import Registrarse from './Componentes/Registrarse';
 import RegistrarseCodigo from './Componentes/RegistrarseCodigo';
-import { ComponentWithContext } from './pages/ComponentWithContext';
 import { HistorialReservas } from './pages/HistorialReservas/HistorialReservas';
 import { UserContext } from './context/userContext'
 import { Stats } from './pages/Stats/Stats'
 
-function Routing() {
-  // const { user } = useContext(UserContext)
-  const user = {user: true, rol: true}
-  console.log('ROUTING USER=>', user)
-  const userLoggedIn = user.user !== null
-  const userIsAdmin = user.rol !== null
+function Routing(props) {
+  const { user } = useContext(UserContext)
+  const userLoggedIn = user.id !== null;
+  const userIsAdmin = user.rol !== null;
   const showUserRoutes = userLoggedIn && !userIsAdmin
   const showAuthRoutes = !userLoggedIn
   const showAdminRoutes = userIsAdmin
 
-  const renderAuthRoutes = () => (
+  const authProps = {
+    userLoggedIn,
+    userIsAdmin
+  }
+
+  const renderAuthRoutes = (authProps) => (
     <>
-      <Route path='/IniciarSesion' element = {<IniciarSesion/>}/>
-      <Route  exact path="/RecuperarContra" element={<Contra imagen = {imagen1} mensaje = "Ingrese su correo"/>}/>
-      <Route exact path="/Registrarse" element={<Registrarse/>}/>
+      <Route path='/IniciarSesion' element = {<IniciarSesion {...authProps} />}/>
+      <Route  exact path="/RecuperarContra" element={<Contra imagen = {imagen1} mensaje = "Ingrese su correo" {...authProps} />}/>
+      <Route exact path="/Registrarse" element={<Registrarse {...authProps} />}/>
     </>
   )
 
-  const renderPublicRoutes = () => (
+  const renderPublicRoutes = (authProps) => (
     <>
-      <Route path='/' element={<Pantallaprincipal/>}/>
-      <Route path='/Reservas' element={<Reservas/>}/>
+      <Route path='/' element={<Pantallaprincipal {...authProps} />}/>
+      <Route path='/Reservas' element={<Reservas {...authProps} />}/>
     </>
   )
 
-  const renderUserRoutes = () => {
+  const renderUserRoutes = (authProps) => {
     return (
       <>
-        <Route  exact path="/Borrarcuenta" element={<Contra imagen = {imagen1} mensaje = "Ingrese su correo"/>}/>
-        <Route  exact path="/GraficasUsuario" element={<Contra imagen = {imagen1} mensaje = "Ingrese su correo"/>}/>
-        <Route  exact path="/estadisticas" element={<Stats />}/>
+        <Route  exact path="/Borrarcuenta" element={<Contra imagen = {imagen1} mensaje = "Ingrese su correo" {...authProps} />}/>
+        <Route  exact path="/GraficasUsuario" element={<Contra imagen = {imagen1} mensaje = "Ingrese su correo" {...authProps} />}/>
+        <Route  exact path="/estadisticas" element={<Stats {...authProps} />}/>
       </>
     )
   }
 
-  const renderAdminRoutes = () => (
+  const renderAdminRoutes = (authProps) => (
     <>
-      <Route  exact path="/historial-reservas" element={<HistorialReservas />}/>
-      <Route  exact path="/estadisticas" element={<Stats />}/>
+      <Route  exact path="/historial-reservas" element={<HistorialReservas {...authProps} />}/>
+      <Route  exact path="/estadisticas" element={<Stats {...authProps} />}/>
     </>
   )
 
   return (
     <>
       <BrowserRouter>
-        <Navbar/>
+        <Navbar2 {...authProps} />
         <Routes>
-          {renderPublicRoutes()}
-          {showUserRoutes && renderUserRoutes()}
-          {showAuthRoutes && renderAuthRoutes()}
-          {showAdminRoutes && renderAdminRoutes()}
+          {renderPublicRoutes(authProps)}
+          {showUserRoutes && renderUserRoutes(authProps)}
+          {showAuthRoutes && renderAuthRoutes(authProps)}
+          {showAdminRoutes && renderAdminRoutes(authProps)}
           <Route path='*' element={<Navigate to="/" />} />
-          {/* EXAMPLE CONTEXT */}
-          <Route  exact path="/user-context" element={<ComponentWithContext />}/>
         </Routes>
       </BrowserRouter>
     </>
